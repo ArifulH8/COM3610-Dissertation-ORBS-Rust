2656b23107800d7fd663b3321869bb59ca1f4c11
Ariful Haque <ArifulH6464@gmail.com>
2024-02-11 10:57:42 +0000

    Add file reading

--- src/slicer.rs
+++ src/slicer.rs
@@ -1,5 +1,18 @@
+use std::error::Error;
+use std::fs;
+
 use crate::Config;
 
 pub fn start(config: Config) {
     println!("Slicer Start");
+
+    let contents = match read_file(config.file_path) {
+        Ok(contents) => contents,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+}
+
+fn read_file(file_path: String) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(file_path)?;
+    Ok(contents)
 }
