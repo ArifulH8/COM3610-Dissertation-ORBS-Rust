515312827a46db4ebbf024fb01fc1c65662de852
Ariful Haque <ArifulH6464@gmail.com>
2024-02-11 19:15:02 +0000

    Arbitrary slicing

--- src/slicer.rs
+++ src/slicer.rs
@@ -13,7 +13,8 @@ pub fn start(config: Config) {
 
     let mut string_lines = file_lines(contents);
     string_lines.reverse();
-    println!("{:?}", string_lines);
+
+    slice(string_lines);
 }
 
 fn read_file(file_path: String) -> Result<String, Box<dyn Error>> {
@@ -24,4 +25,34 @@ fn read_file(file_path: String) -> Result<String, Box<dyn Error>> {
 fn file_lines(contents: String) -> Vec<String> {
     let lines: Vec<String> = contents.split("\n").map(|s| s.to_string()).collect();
     lines
+}
+
+fn slice(mut file_vec: Vec<String>) {
+    // let vec_lenth = file_vec.len();
+    let mut counter = 0;
+    while counter < file_vec.len() {
+        println!("{}", counter);
+        let file_vec_clone = file_vec.clone();
+
+        let result = delete(file_vec_clone, counter);
+
+        if result {
+            file_vec.remove(counter);
+        }
+
+        counter += 1;
+    }
+
+    println!("{:?}", file_vec);
+}
+
+fn delete(_file_vec: Vec<String>, counter: usize) -> bool {
+    // let mut _bestDW = 0
+    // let mut succeed = false;
+
+    if counter % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
 }
