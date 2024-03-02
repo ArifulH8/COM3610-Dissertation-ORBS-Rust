952848f3b8569aac9b4d55e7fc7b49e1d34de291
Ariful Haque <ArifulH6464@gmail.com>
2024-03-02 19:44:51 +0000

    Reformat index.test.js

--- examples/ex_js/src/index.test.js
+++ examples/ex_js/src/index.test.js
@@ -1,6 +1,6 @@
- import { expect, test } from 'vitest'
-import { hello_name } from './index'
+import { expect, test } from 'vitest';
+import { hello_name } from './index';
 
 test('run hello_name with Ariful ', () => {
-  expect(hello_name("Ariful")).toBe(`Hello World and hello Ariful`)
-})
+  expect(hello_name("Ariful")).toBe(`Hello World and hello Ariful`);
+});
